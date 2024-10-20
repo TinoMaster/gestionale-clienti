d40c30bbec090587ce2415630b3ea12a6a953b6a
TinoMaster <90280253+TinoMaster@users.noreply.github.com>
2024-10-20 20:02:20 +0200

    added angular material, and added icons to the sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1abc9c",
+        secondary: "#34495e",
+        dark: "#061728",
+        dark_light: "#2c3e50",
+      },
+    },
   },
   plugins: [],
 };
